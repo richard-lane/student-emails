bb01e1679c2011101c15ba6e7b0faf74ee27395f
Richard Lane <richard.lane@bristol.ac.uk>
2022-06-15 10:56:57 +0100

    initial commit

---
+++ libEmails/read.py
@@ -0,0 +1,16 @@
+"""
+Read emails
+
+"""
+import pandas as pd
+
+
+def read_emails(path: str) -> pd.DataFrame:
+    """
+    Read an Excel spreadsheet of emails located at `path` into a pandas DataFrame
+
+    :param path: location of the student emails Excel file
+    :returns: dataframe holding all the emails + metadata
+
+    """
+    ...
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Student emails project
+
+Skel at the moment
+
+"""
+from libEmails import read
+
+
+def main() -> None:
+    read.read_emails("")
+
+
+if __name__ == "__main__":
+    main()
