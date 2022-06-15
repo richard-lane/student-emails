6faff0dc20b53e9692bfec4ce3ca217b0405afde
Richard Lane <richard.lane@bristol.ac.uk>
2022-06-15 11:38:36 +0100

    bag of words for email bodies

--- libEmails/read.py
+++ libEmails/read.py
@@ -5,12 +5,22 @@ Read emails
 import pandas as pd
 
 
-def read_emails(path: str) -> pd.DataFrame:
+def read_email_body() -> pd.DataFrame:
     """
-    Read an Excel spreadsheet of emails located at `path` into a pandas DataFrame
+    Read the Excel spreadsheet of emails with email bodies
+
+    Replaces NaN email bodies with empty str
+
+    Reads the first sheet
+    Uses the first row as column labels
+    Parses all columns
 
     :param path: location of the student emails Excel file
     :returns: dataframe holding all the emails + metadata
 
     """
-    ...
+    rv = pd.read_excel(
+        "./data/Anonymised Electrical & Electronic Engineering Email Body Sample 200.xlsx"
+    )
+    rv["EmailBody"] = rv["EmailBody"].fillna("")
+    return rv
--- main.py
+++ main.py
@@ -4,11 +4,18 @@ Student emails project
 Skel at the moment
 
 """
+import pandas as pd
 from libEmails import read
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def main() -> None:
-    read.read_emails("")
+    # Read email bodies into a pandas dataframe
+    email_bodies: pd.DataFrame = read.read_email_body()
+
+    # Read the email bodies into a bag of words
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(email_bodies.EmailBody)
 
 
 if __name__ == "__main__":
