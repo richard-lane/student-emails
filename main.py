809c9771ec20048560b11896f50fcc17f5adaadc
Richard Lane <richard.lane@bristol.ac.uk>
2022-06-24 17:46:50 +0100

    train test split using a np boolean mask

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ Skel at the moment
 
 """
 import pandas as pd
+import numpy as np
 from typing import Tuple
 from sklearn import metrics
+from scipy import sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
@@ -17,6 +19,11 @@ def main() -> None:
     # Read email subjects and demand categories into pandas series
     subjects, categories = read.read_email_subjects()
 
+    # Mask for training
+    rng = np.random.default_rng(seed=0)
+    train_fraction = 0.75
+    train = rng.random(len(subjects)) < train_fraction
+
     # Read the email bodies into a bag of words
     vectorizer = TfidfVectorizer(
         sublinear_tf=True,
@@ -26,13 +33,14 @@ def main() -> None:
         ngram_range=(1, 2),
         stop_words="english",
     )
-    X = vectorizer.fit_transform(subjects)
+    X: sparse.csr_matrix = vectorizer.fit_transform(subjects)
 
     # Classifier
     clf = MultinomialNB()
-    clf.fit(X, categories)
+    clf.fit(X[train], categories[train])
 
-    print(metrics.classification_report(categories, clf.predict(X)))
+    print(metrics.classification_report(categories[train], clf.predict(X[train])))
+    print(metrics.classification_report(categories[~train], clf.predict(X[~train])))
 
 
 if __name__ == "__main__":
