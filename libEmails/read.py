d8ea2b5663654b263783dafbd5eca37b8875faf7
Richard Lane <richard.lane@bristol.ac.uk>
2022-06-25 14:28:15 +0100

    return bag of words instead of dataframe (broke the interactive bit slightly)

--- libEmails/read.py
+++ libEmails/read.py
@@ -5,6 +5,7 @@ Read emails
 import pandas as pd
 import numpy as np
 from typing import Tuple
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def read_email_body() -> pd.DataFrame:
@@ -31,7 +32,7 @@ def read_email_body() -> pd.DataFrame:
 
 
 def read_email_subjects(
-    min_support: int = 15, verbose: bool = True
+    min_support: int = 15, verbose: bool = True, sampling: str = "naive"
 ) -> Tuple[pd.Series, pd.Series]:
     """
     Read the Excel spreadsheet of emails with email subject lines only
@@ -43,10 +44,17 @@ def read_email_subjects(
 
     :param min_support: minimum number of emails with in a given category. Defaults to 15.
     :param verbose: whether to print information about what cuts are being performed.
+    :param sampling: sampling strategy; "naive", "undersample" or "oversample".
+                     Naive sampling makes no attempt to balance the data.
+                     Undersampling resamples all demand types except the minority class.
+                     Oversampling uses SMOTE to oversample all classes but the majority.
+
     :returns: pandas series of email subject lines
     :returns: pandas series of email demand categories
 
     """
+    assert sampling in {"naive", "undersample", "oversample"}
+
     df: pd.DataFrame = pd.read_excel(
         "./data/Anonymised_UG_Economics_Email_Sample_7500_2022-05-26 Strip.xlsx"
     )
@@ -73,4 +81,19 @@ def read_email_subjects(
     mask = ~np.logical_or.reduce([df[category_heading] == s for s in too_few.keys()])
     df = df[mask]
 
-    return df[subject_heading], df[category_heading]
+    # Series for our class labels
+    labels = df[category_heading]
+
+    # Convert our subject lines into a bag of words
+    subjects = df[subject_heading]
+    vectorizer = TfidfVectorizer(
+        sublinear_tf=True,
+        min_df=3,
+        norm="l2",
+        encoding="latin-1",
+        ngram_range=(1, 2),
+        stop_words="english",
+    )
+    bag_of_words: sparse.csr_matrix = vectorizer.fit_transform(subjects)
+
+    return bag_of_words, labels
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import numpy as np
 from typing import Tuple
 from sklearn import metrics
 from scipy import sparse
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 from libEmails import read
@@ -18,23 +17,12 @@ from libEmails import read
 
 def main() -> None:
     # Read email subjects and demand categories into pandas series
-    subjects, categories = read.read_email_subjects()
+    X, categories = read.read_email_subjects()
 
     # Mask for training
     rng = np.random.default_rng(seed=0)
     train_fraction = 0.75
-    train = rng.random(len(subjects)) < train_fraction
-
-    # Read the email bodies into a bag of words
-    vectorizer = TfidfVectorizer(
-        sublinear_tf=True,
-        min_df=3,
-        norm="l2",
-        encoding="latin-1",
-        ngram_range=(1, 2),
-        stop_words="english",
-    )
-    X: sparse.csr_matrix = vectorizer.fit_transform(subjects)
+    train = rng.random(X.shape[0]) < train_fraction
 
     # Classifier
     clf = MultinomialNB()
